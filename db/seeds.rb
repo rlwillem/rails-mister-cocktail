91615adc49dc3425186dcaa96c82f0967f4450d5
Willem Loobuyck <willemloobuyck@msn.com>
2018-11-22 18:32:19 +0900

    added some style

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,15 @@ require 'json'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients_serialized = open(url).read
+puts 'adding ingredients, brewing cocktails'
 ingredients = JSON.parse(ingredients_serialized)
 ingredients["drinks"].each do |ingredient|
   cocktail_ingredient = Ingredient.new(name: ingredient["strIngredient1"])
   cocktail_ingredient.save
 end
+
+
+puts 'Finished!'
